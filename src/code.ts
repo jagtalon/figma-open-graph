d3f428152c83d66238363d37c07df16b8632a21c
Jag Talon <jag@jagtalon.com>
2020-06-14 22:11:20 -0400

    Initial commit for doing a network request.

--- src/code.ts
+++ src/code.ts
@@ -1,33 +1,14 @@
-// This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+figma.showUI(__html__, { visible: false })
+figma.ui.postMessage({ type: 'networkRequest' })
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
+figma.ui.onmessage = async (msg) => {
+  const text = figma.createText()
+  // Make sure the new text node is visible where we're currently looking
+  text.x = figma.viewport.center.x
+  text.y = figma.viewport.center.y
 
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+  await figma.loadFontAsync(text.fontName as FontName)
+  text.characters = msg
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
+  figma.closePlugin()
+}
--- src/ui.ts
+++ src/ui.ts
@@ -1,11 +1,14 @@
 import './ui.css'
 
-document.getElementById('create').onclick = () => {
-  const textbox = document.getElementById('count') as HTMLInputElement
-  const count = parseInt(textbox.value, 10)
-  parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
-}
-
-document.getElementById('cancel').onclick = () => {
-  parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
-}
+window.onmessage = async (event) => {
+    if (event.data.pluginMessage.type === 'networkRequest') {
+      var request = new XMLHttpRequest()
+      // This link has random lorem ipsum text
+      request.open('GET', 'https://figma-open-graph-scraper.glitch.me/spotify.com')
+      request.responseType = 'text'
+      request.onload = () => {
+        window.parent.postMessage({pluginMessage: request.response}, '*')
+      };
+      request.send()
+    }
+  }
