e4751b04c395062e5dc6cb0b6e4ad0f081210b4b
Jag Talon <jag@jagtalon.com>
2020-06-16 13:50:21 -0400

    Add Figma DS to style the components

--- src/code.ts
+++ src/code.ts
@@ -1,14 +1,12 @@
-figma.showUI(__html__, { visible: false })
+/* 
+  This code is specific to Figma.
+  It doesn't have any access to browser APIs, but it does
+  have access to the Figma document itself and Figma APIs.
+*/
+
+figma.showUI(__html__)
 figma.ui.postMessage({ type: 'networkRequest' })
 
 figma.ui.onmessage = async (msg) => {
-  const text = figma.createText()
-  // Make sure the new text node is visible where we're currently looking
-  text.x = figma.viewport.center.x
-  text.y = figma.viewport.center.y
-
-  await figma.loadFontAsync(text.fontName as FontName)
-  text.characters = msg
-
-  figma.closePlugin()
+  
 }
--- src/ui.ts
+++ src/ui.ts
@@ -1,14 +1,21 @@
+/*
+    This code will live in the <iframe> within Figma.
+    It has access to all the browser APIs.
+*/
+
+import './libs/figma-ds/figma-plugin-ds.css'
+import './libs/figma-ds/figma-plugin-ds.js'
 import './ui.css'
 
 window.onmessage = async (event) => {
     if (event.data.pluginMessage.type === 'networkRequest') {
-      var request = new XMLHttpRequest()
-      // This link has random lorem ipsum text
-      request.open('GET', 'http://localhost:8080/spotify.com')
-      request.responseType = 'text'
-      request.onload = () => {
+        var request = new XMLHttpRequest()
+
+        request.open('GET', 'http://localhost:8080/spotify.com')
+        request.responseType = 'text'
+        request.onload = () => {
         window.parent.postMessage({pluginMessage: request.response}, '*')
-      };
-      request.send()
+        };
+        request.send()
     }
-  }
+}
