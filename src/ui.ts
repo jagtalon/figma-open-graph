68722f784e1a5910316cb807b2c09a39bda67065
Jag Talon <jag@jagtalon.com>
2020-06-17 10:06:12 -0400

    Organize code and add comments

--- src/ui.ts
+++ src/ui.ts
@@ -7,11 +7,11 @@ import './libs/figma-ds/figma-plugin-ds.css'
 import './libs/figma-ds/figma-plugin-ds.js'
 import './ui.css'
 
-let serverUrl = 'http://localhost:8080/';
-let crawlUrl: HTMLInputElement = document.querySelector('.url input');
-let submitButton: HTMLButtonElement = document.querySelector('.url button');
+const pluginServer = 'http://localhost:8080/';
 
-// Add event handlers on each example tag.
+// Example buttons
+// These provide example URLs that the user can use to try out the plugin.
+let crawlUrl: HTMLInputElement = document.querySelector('.url input');
 let exampleTags = document.querySelectorAll('.examples span').forEach((example: HTMLSpanElement) => {
     example.addEventListener('click', (event: MouseEvent) => {
         console.log(example.dataset.url);
@@ -21,12 +21,15 @@ let exampleTags = document.querySelectorAll('.examples span').forEach((example: 
 });
 
 // Submit button
+// Once this is clicked, the plugin sends the URL to the server and waits for
+// a response back.
+let submitButton: HTMLButtonElement = document.querySelector('.url button');
 submitButton.addEventListener('click', () => {
     var request = new XMLHttpRequest()
 
     if(crawlUrl.value.length > 0) {
-        request.open('GET', serverUrl + crawlUrl.value);
-        request.responseType = 'text';
+        request.open('GET', pluginServer + crawlUrl.value);
+        request.responseType = 'json';
         request.onload = () => {
             renderElements(request.response);
         };
